5262d699307337cb15e20244612efa69ba1f574c
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-08-06 11:28:59 -0500

    updated routes

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('about', views.about, name="about")
+    path('index.html', views.index, name="index"),
+    path('about.html', views.about, name="about")
 ]
