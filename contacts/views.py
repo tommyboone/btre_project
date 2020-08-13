49bcd80852ff0d1ab19bc540c91c69eeae707cb4
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-08-13 14:06:34 -0500

    removed email function for now

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from .models import Contact
 from django.views.decorators.csrf import csrf_protect
 # Create your views here.
@@ -27,13 +27,13 @@ def contact(request):
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
     contact.save()
 
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry made for ' + listing + '. Sign into the admin panel for more info.',
-      'tommyboone23@gmail.com',
-      [realtor_email, 'tommyboone23@gmail.com'],
-      fail_silently=False
-    )
+    # send_mail(
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry made for ' + listing + '. Sign into the admin panel for more info.',
+    #   'tommyboone23@gmail.com',
+    #   [realtor_email, 'tommyboone23@gmail.com'],
+    #   fail_silently=False
+    # )
 
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon.')
